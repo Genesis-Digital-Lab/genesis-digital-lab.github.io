67a56d73166d2fd8c5a55ab3c3730c9cafa105ba
Genesis Digital Lab <149869254+Genesis-Digital-Lab@users.noreply.github.com>
2025-01-15 03:07:45 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,23 @@
+var app = angular.module("genesis", []);
+app.controller("myCtrl", function($scope) {
+
+
+});
+
+
+
+function loadHeader() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            console.log(data)
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('event')
+    loadHeader();
+});
