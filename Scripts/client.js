1f853b554d72213a20dbeef1d18203e0ca8e90b1
Genesis-Digital-Lab <digitalsolutions.genesis@gmail.com>
2025-02-02 02:18:08 +0530

    First commit

---
+++ Scripts/client.js
@@ -0,0 +1,29 @@
+var app = angular.module("genesis", []);
+app.controller("myCtrl", function($scope) {
+
+
+});
+
+//this is client side logic 
+//this is client side logic 
+function loadHeader() {
+
+const SECRET_KEY = "{{SECRET_KEY_PLACEHOLDER}}";
+
+// Use SECRET_KEY in your logic
+console.log("The secret key is: ", SECRET_KEY);
+
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            console.log(data)
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('event')
+    loadHeader();
+});
