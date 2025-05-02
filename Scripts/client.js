cb67eb4f2fc2c81b2186d7c533936cf17993d26e
Genesis-Digital-Lab <digitalsolutions.genesis@gmail.com>
2025-05-02 08:12:54 +0530

    dark theme update

--- Scripts/client.js
+++ Scripts/client.js
@@ -1,5 +1,5 @@
 var app = angular.module("genesis", []);
-app.controller("myCtrl", function($scope) {
+app.controller("genesis-controller", function($scope) {
 
     $scope.carouselObj = [{
         "id":1,
@@ -26,27 +26,27 @@ app.controller("myCtrl", function($scope) {
 
       $scope.helpfulLinks = [{
         "title": "JSON Formatter",
-        "href": "json_formatter.html",
+        "href": "Formatter/JSON/json_formatter.html",
         "target": "_blank"
       },
       {
         "title": "JSON - CSV Convertor.",
-        "href": "json_csv_conversion.html",
+        "href": "Formatter/JSON/json_csv_conversion.html",
         "target": "_blank"
       },
       {
         "title": "Image Editor",
-        "href": "image_editor.html",
+        "href": "Formatter/JSON/json_csv_conversion.html",
         "target": "_blank"
       },
       {
         "title": "Code Complier",
-        "href": "json_formatter.html",
+        "href": "Formatter/JSON/json_formatter.html",
         "target": "_blank"
       },
       {
-        "title": "Image Editor",
-        "href": "json_formatter.html",
+        "title": "JSON Visualizer",
+        "href": "Formatter/JSON/json_visualizer.html",
         "target": "_blank"
       }
     ]
@@ -54,8 +54,7 @@ app.controller("myCtrl", function($scope) {
 
 });
 
-//this is client side logic 
-//this is client side logic 
+
 function loadHeader() {
 
 const SECRET_KEY = "{{SECRET_KEY_PLACEHOLDER}}";
@@ -66,8 +65,16 @@ console.log("The secret key is: ", SECRET_KEY);
     fetch('/src/header.html')
         .then(response => response.text())
         .then(data => {
-            console.log(data)
+            //console.log(data)
             document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+
+        fetch('/src/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            //console.log(data)
+            document.getElementById('footer').innerHTML = data;
         })
         .catch(error => console.error('Error loading header:', error));
 }
@@ -77,3 +84,65 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('event')
     loadHeader();
 });
+
+
+// Theme toggle script Starts Here
+const storageKey = 'theme-preference'
+
+const onClick = () => {
+  // flip current value
+  theme.value = theme.value === 'light'
+    ? 'dark'
+    : 'light'
+
+  setPreference()
+}
+
+const getColorPreference = () => {
+  if (localStorage.getItem(storageKey))
+    return localStorage.getItem(storageKey)
+  else
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light'
+}
+
+const setPreference = () => {
+  localStorage.setItem(storageKey, theme.value)
+  reflectPreference()
+}
+
+const reflectPreference = () => {
+  document.firstElementChild
+    .setAttribute('data-theme', theme.value)
+
+  document
+    .querySelector('#theme-toggle')
+    ?.setAttribute('aria-label', theme.value)
+}
+
+const theme = {
+  value: getColorPreference(),
+}
+
+// set early so no page flashes / CSS is made aware
+reflectPreference()
+
+window.onload = () => {
+  // set on load so screen readers can see latest value on the button
+  reflectPreference()
+
+  // now this script can find and listen for clicks on the control
+  document
+    .querySelector('#theme-toggle')
+    .addEventListener('click', onClick)
+}
+
+// sync with system changes
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', ({matches:isDark}) => {
+    theme.value = isDark ? 'dark' : 'light'
+    setPreference()
+  })
+// Theme toggle script Ends Here
